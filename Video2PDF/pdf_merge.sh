5215e7b005846caaac0cd452e3fe65d47e3b066e
Wiseman Lim <Wiseman.Lim@gmail.com>
2023-07-07 18:07:43 +0900

    Update echo on pdf_merge.sh

--- Video2PDF/pdf_merge.sh
+++ Video2PDF/pdf_merge.sh
@@ -1,4 +1,4 @@
 cd /Volumes/MyWorks_WD/Documents/PycharmProjects/Examples-Python/Video2PDF
 source ../venv/bin/activate
-python3 video2pdf.py --filename "*.pdf" --env skip --split skip --merge full
+python3 video2pdf.py --filename "split/*.pdf" --env skip --split skip --merge full
 deactivate
