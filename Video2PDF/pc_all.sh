862c548cd5733c5cfc655c23ffca7568bd3c9267
Wiseman Lim <Wiseman.Lim@gmail.com>
2023-07-23 12:05:38 +0900

    Update pc_all.sh

--- Video2PDF/pc_all.sh
+++ Video2PDF/pc_all.sh
@@ -1,1 +1,9 @@
-find ./in -type f -name "*.mov" -exec sh -c 'sh pdf_convert.sh "$(basename "{}" )"' \;
+#!/bin/bash
+# find ./in -type f -name "*.mov" -exec sh -c 'sh pdf_convert.sh "$(basename "{}" )"' \; -o -exec sudo mv "{}" ~/.Trash/ \;
+# for file in $(find ./in -type f -name "*.mov");
+find . -name "*.mov" -print0 | while read -d $'\0' file
+do
+	echo "Convert : [ "$file" ] => [ $(basename "$file") ]"
+	sh pdf_convert.sh "$(basename "$file")"
+	mv "$file" ~/.Trash/.
+done
