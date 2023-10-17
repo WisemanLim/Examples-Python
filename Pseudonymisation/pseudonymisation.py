10c89b422014054cafe1090c09fb45efe25d43d4
Wiseman Lim <Wiseman.Lim@gmail.com>
2023-10-17 21:00:36 +0900

    Update pseudonymisation.py
    Columns 가변처리를 위한 로직 수정 완료

--- Pseudonymisation/pseudonymisation.py
+++ Pseudonymisation/pseudonymisation.py
@@ -39,32 +39,52 @@ def generateerateKey2Base64(name, gender, age):
 
     return value
 
+ # Columns 적용 로직 수정 필요
+def generateKey2HexaColumns(columns = None):
+    # ex) 홍길동01234456789abcdefm56
+    result = "".join(f"{col}" for col in columns)
+    result += f"{_SALT_}"
+    # print(result)
+
+    # 해시값 생성
+    # hash = hashlib.sha224(result.encode())
+    hash = hashlib.sha256(result.encode())
+    # hash = hashlib.sha512(result.encode())
+
+    # 헥사 인코딩
+    value = hash.hexdigest().lower()
+    # value = hash.hexdigest().upper()
+
+    return value
+
 def main(param=None):
     filename = param.filename
     salt = param.salt
     if (salt is not None): _SALT_ = salt
     with open('salt.salt', 'w', encoding='utf-8') as f: f.write(_SALT_)
     merge_key = param.columns
-    merge_key = merge_key.split(',') # Columns 적용 로직 수정 필요
+    merge_key = merge_key.split(',')
+    # print(merge_key)
 
     # generateerateKey2Base64('강원권', '', '48')
     # generateKey2Hexa('강원권', '', '48')
 
     # 원본 파일 적재, default : utf-8
     df = pd.read_csv(filename)
-    print(df.head(3))
+    # print(df.head(3))
 
     # 결합키 생성
     # 'A', 'B', 'C' 컬럼(속성)에 대하여 결합키를 생성하여 ‘merge_key key’ 컬럼(속성)에 결합키 추가
     # 컬럼의 마지막에 'merge_key key' 컬럼 추가
     # df['merge_key'] = df.apply(lambda df:generateKey2Hexa(df['A'], df['B'], df['C']), axis=1)
     # 지정한 컬럼의 순번(‘ 에 'merge_key key’ 컬럼 추가
-    df.insert(0, 'merge_key', df.apply(lambda df:generateKey2Hexa(df['이름'], df['성별'], df['나이']), axis=1))
+    # df.insert(0, 'merge_key', df.apply(lambda df:generateKey2Hexa(df['이름'], df['성별'], df['나이']), axis=1))
+    df.insert(0, 'merge_key', df.apply(lambda df:generateKey2HexaColumns(df[col] for col in merge_key), axis=1))
 
     # 일련번호 생성
     # df['serial_no'] = df.reset_index().index+1
     df.insert(1, 'serial_no', df.reset_index().index+1)
-    df.head(3)
+    print(df.head(3))
 
     # 결합키와 일련번호를 추출한 dataframe
     df_key = df[['merge_key', 'serial_no']]
