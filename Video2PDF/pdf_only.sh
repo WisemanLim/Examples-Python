e4cad036f4e84ae8aa8b756fce1ee85e8b214f4e
Wiseman Lim <Wiseman.Lim@gmail.com>
2023-11-02 18:51:07 +0900

    Add and Modify for pdf convert

--- Video2PDF/pdf_only.sh
+++ Video2PDF/pdf_only.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /Volumes/MyWorks_WD/Documents/PycharmProjects/Examples-Python/Video2PDF
 source ../venv/bin/activate
-python3 video2pdf.py --filename "split/*.pdf" --env skip --merge full
+python3 video2pdf.py --filename in --env pdf
 deactivate
